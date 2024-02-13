47673ddf641e481dd92133f6a0dd560ade80df43
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2024-02-13 17:39:55 -0300

    Create - M1S06Ex5
    Create - Enums "StatusMatricula" e "StatusNivel" em atenção ao requisito do Card n° 5, do projeto M1S06.
    
    <Resp: José André - Ref #Card>

---
+++ src/enums/StatusMatricula.java
@@ -0,0 +1,14 @@
+// TODO: 13/02/2024
+//  Crie um enum para representar Status de Matricula e adicione ele como atributo a Aluno
+//  Valores: ATIVO, TRANCADO, FORMADO.
+//  Crie um enum para representar o Cargo do Funcionário.
+//  Valores: Iniciante, Experiente, Avançado
+//  Demonstrar o uso de values() e valueOf().
+
+package enums;
+
+public enum StatusMatricula {
+    ATIVO,
+    TRANCADO,
+    FORMADO
+}
