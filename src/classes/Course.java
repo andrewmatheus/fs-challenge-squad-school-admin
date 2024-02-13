384419ca532a621b78e5c2f0c78d4f7ea0de1ad2
Michel Vieira <michel@darochavieira.com>
2024-02-13 18:25:59 -0300

    encapsulamento

--- src/classes/Course.java
+++ src/classes/Course.java
@@ -20,5 +20,17 @@ public class Course {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
     // </editor-fold>
 }
