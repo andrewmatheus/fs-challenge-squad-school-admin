24443ac411601ad84d0d3cff4c20506de338bebc
andrewmatheus <andrewmatheus@gmail.com>
2024-02-13 04:00:54 -0300

    feat: Card 2- Create the classes for main entities

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import classes.Class;
+import classes.Course;
+import classes.Student;
+import classes.Teacher;
+
+public class Main {
+    public static void main(String[] args) {
+        // Usage example
+
+        // Creating a teacher
+        Teacher teacher = new Teacher("André", 30, 10);
+
+        // Creating a course
+        Course course = new Course("BackEnd-Java", teacher);
+        Course courseTwo = new Course("FrontEnd-Angular", teacher);
+
+        // Creating a class with course
+        Class class2024 = new Class(2024, course);
+
+        // Creating a students
+        Student studentOne = new Student("Gabriel");
+        Student studentTwo = new Student("Samuel");
+
+        // add students a class2024
+        class2024.addStudent(studentOne);
+        class2024.addStudent(studentTwo);
+
+        // list students the class2024
+        System.out.println("\n+---------------------------+");
+        class2024.listStudents();
+
+        // add courses for student
+        studentOne.addCourse(course);
+        System.out.println("\n+---------------------------+");
+        studentOne.listCourses();
+
+        studentTwo.addCourse(courseTwo);
+        System.out.println("\n+---------------------------+");
+        studentTwo.listCourses();
+
+    }
+}
