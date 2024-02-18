6bad7923c63cf6cd6b7acefec084bb107d8c19c7
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2024-02-18 09:13:06 -0300

    Refactor - StudentsData.java
    Improving method descriptions.
    
    <Resp: José André - Ref #Card-8.3>

--- src/data/StudentsData.java
+++ src/data/StudentsData.java
@@ -8,9 +8,16 @@ public class StudentsData {
 
     private static ArrayList<Student> studentsList = new ArrayList<>();
 
+    /*
+     * Method addStudent - responsible for adding a student to the list.
+     * */
         public static void addStudent(Student student) {
         studentsList.add(student);
     }
+
+    /*
+     * Method removeStudent - responsible for removing a student from the list.
+     * */
     public static void removeStudent(int id) {
         try {
             Student removedStudent = studentsList.remove(id);
@@ -20,6 +27,10 @@ public class StudentsData {
             System.out.println("Invalid ID. Student could not be removed.");
         }
     }
+
+    /*
+     * Method findStudent - responsible for finding a student on the list.
+     * */
     public static Student findStudent(int id){
         if (id >= 0 && id < studentsList.size()) {
             return studentsList.get(id);
